3257a59ef2e8cde3f98bef707ab6a07ad02dc10f
lottt105 <hyobin0601@naver.com>
2023-12-11 02:03:57 +0900

    feat: 프로필 기능 및 화면 구현

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,139 @@
+import styled from "styled-components";
+import Tweet from "../components/tweet";
+import { useEffect, useState } from "react";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { TweetType } from "../models/tweet";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  overflow-y: scroll;
+`;
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  min-height: 80px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #e4e5ec;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const Name = styled.span`
+  font-size: 22px;
+`;
+
+const Tweets = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+  background-color: white;
+  border-radius: 10px;
+  overflow-y: scroll;
+`;
+
 export default function Profile() {
-  return <></>;
+  const user = auth.currentUser;
+  const [userPhoto, setUserPhoto] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<TweetType[]>([]);
+
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setUserPhoto(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc"),
+      where("userId", "==", user?.uid),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {userPhoto ? (
+          <AvatarImg src={userPhoto} />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
+    </Wrapper>
+  );
 }
