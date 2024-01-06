229a33d8b63866e3cdd38bd5573af943d2fe7bb4
lottt105 <hyobin0601@naver.com>
2024-01-06 18:53:19 +0900

    feat: loading 스타일 수정

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,4 +1,13 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const load = keyframes`
+  0% {
+      transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,14 +16,22 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Text = styled.span`
-  font-size: 24px;
+const LoadingCircle = styled.div`
+  width: 50px;
+  height: 50px;
+  margin: 10px auto;
+
+  border: 10px solid #e3e3e3;
+  border-bottom: 10px solid #000000;
+  border-radius: 50%;
+
+  animation: ${load} 1.5s linear infinite;
 `;
 
 export default function Loading() {
   return (
     <Wrapper>
-      <Text>Loading</Text>
+      <LoadingCircle />
     </Wrapper>
   );
 }
